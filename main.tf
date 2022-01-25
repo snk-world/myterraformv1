cc363d26c7d4b82d763e031196630056c72cfa87
ilia <ilia.vaster@gmail.com>
2022-01-25 22:19:16 +0300

    v1.77

--- main.tf
+++ main.tf
@@ -71,10 +71,14 @@ output "external_ip_address_vm_1" {
 }
 
 output "internal_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
 }
 
 output "external_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_LB" {
+  value = yandex_lb_network_load_balancer.foo.id
 }
 
